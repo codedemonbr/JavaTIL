31b559a36835a441c3076960d1e8663f37ded181
Henrique <henrique.thsantos.ths@gmail.com>
2021-01-10 17:38:51 -0300

    Inferencia

---
+++ exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,27 @@
+package fundamentos;
+
+public class Inferencia {
+	public static void main(String[] args) {
+		double a = 4.5;
+		System.out.println(a);
+		
+		var b = 4.5;
+		System.out.println(b);
+		
+		var c = "Um texto";
+		System.out.println(c);
+		
+		c = "Outro texto";
+		System.out.println(c);
+		
+//		c = 5.6;
+		
+		double d;
+		d = 123.65;
+		System.out.println(d);
+		
+		var e = 123.45;
+		System.out.println(e);
+		
+	}
+}
