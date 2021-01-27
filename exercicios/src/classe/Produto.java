4218481fc8913aa98c9e06f34dc59eb5a564c6a1
Henrique <henrique.thsantos.ths@gmail.com>
2021-01-27 14:41:44 -0300

    Variáveis estáticas, valor e referencia

--- exercicios/src/classe/Produto.java
+++ exercicios/src/classe/Produto.java
@@ -3,23 +3,27 @@ package classe;
 public class Produto {
 	String nome;
 	double preco;
-	double desconto;
+	static double desconto = 0.25;
 
 	Produto(String nomeInicial) {
 		nome = nomeInicial;
 	}
 
-	Produto(String nomeInicial, double precoInicial, double descontoInicial) {
+	Produto(String nomeInicial, double precoInicial) {
 		nome = nomeInicial;
 		preco = precoInicial;
-		desconto = descontoInicial;
 	}
 
-	Produto(){
-		
+	Produto() {
+
 	}
 
 	double PrecoComDesconto() {
-		return this.preco * (1 - this.desconto);
+		return this.preco * (1 - desconto);
 	}
+
+	double precoComDesconto(double descontoDoGerente) {
+		return preco * (1 - desconto + descontoDoGerente);
+	}
+
 }
--- exercicios/src/classe/ProdutoTest.java
+++ exercicios/src/classe/ProdutoTest.java
@@ -2,20 +2,21 @@ package classe;
 
 public class ProdutoTest {
 	public static void main(String[] args) {
-		Produto p1 = new Produto("Notebook", 4532.65, 0.25);
+		Produto p1 = new Produto("Notebook", 4532.65);
 
 		var p2 = new Produto();
 		p2.nome = "Caneta";
 		p2.preco = 1.65;
-		p2.desconto = 0.29;
 
-		System.out.println(p1.nome);
-		System.out.println(p2.nome);
+		Produto.desconto = 0.5;
+
+		System.out.println(p1.nome + " " + p1.PrecoComDesconto());
+		System.out.println(p2.nome + " " + p2.PrecoComDesconto());
 
 		double precoFinal1 = p1.PrecoComDesconto();
 		double precoFinal2 = p2.PrecoComDesconto();
 		double mediaCarrinho = (precoFinal1 + precoFinal2) / 2;
 
-		System.out.printf("Média do carrinho = R$%.2f.", mediaCarrinho);
+		System.out.printf("Média do carrinho = R$%.2f", mediaCarrinho);
 	}
 }
